64a2d1368f028b25d4d9ff35612f0b77a4fe7be1
Kerri Knier <kerriknier@gmail.com>
2023-08-12 00:19:22 +0100

    build and push nginx image to ecr

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "aws_ecr" {
-  name                 = "platform-training"
+  name                 = "platform-training-kerginx"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
