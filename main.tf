ea53fa8b8ec22c90db490410d97c1bedf40b582f
Kerri Knier <kerriknier@gmail.com>
2023-08-12 00:35:57 +0100

    fix: ECR images should be set as immutable to prevent code injection through image mutation

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_ecr_repository" "aws_ecr" {
   name                 = "platform-training-kerginx"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
