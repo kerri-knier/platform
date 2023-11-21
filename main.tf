2909b754d8d76c6baacc16d73376d52889f31ec7
Kerri Knier <kerriknier@gmail.com>
2023-11-21 22:09:37 +0000

    check if capacity provider was needed

--- main.tf
+++ main.tf
@@ -82,14 +82,14 @@ resource "aws_ecs_service" "ecs_service" {
   }
 }
 
-resource "aws_ecs_cluster_capacity_providers" "aws_capacity_provider" {
-  cluster_name = aws_ecs_cluster.aws_ecs.name
+# resource "aws_ecs_cluster_capacity_providers" "aws_capacity_provider" {
+#   cluster_name = aws_ecs_cluster.aws_ecs.name
 
-  capacity_providers = ["FARGATE"]
+#   capacity_providers = ["FARGATE"]
 
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
-  }
-}
+#   default_capacity_provider_strategy {
+#     base              = 1
+#     weight            = 100
+#     capacity_provider = "FARGATE"
+#   }
+# }
