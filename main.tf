8bfaf0f21e49045c1f57f8caf9edec655af347cb
Kerri Knier <kerriknier@gmail.com>
2023-11-30 22:32:50 +0000

    turn on https for public resources

--- main.tf
+++ main.tf
@@ -132,8 +132,8 @@ resource "aws_lb_target_group" "ecs_lb_target" {
 
 resource "aws_lb_listener" "ecs_lb_listener" {
   load_balancer_arn = aws_lb.ecs_lb.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
@@ -210,8 +210,8 @@ resource "aws_cloudfront_distribution" "ecs_distribution" {
 
     custom_origin_config {
       http_port              = 80
-      https_port             = 80
-      origin_protocol_policy = "http-only"
+      https_port             = 443
+      origin_protocol_policy = "redirect-to-https"
       origin_ssl_protocols   = ["TLSv1"]
     }
   }
@@ -223,7 +223,7 @@ resource "aws_cloudfront_distribution" "ecs_distribution" {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = aws_lb.ecs_lb.id
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
